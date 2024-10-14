6c46c06ff9a35d7b05c69f9963424e24798b821b
Papdi Gergő <papdi.gergo@diak.szbi-pg.hu>
2024-10-14 09:18:38 +0200

    5.feladat

---
+++ 06_feladat.py
@@ -0,0 +1,12 @@
+#fsz_input = int(input("Kérlek adj meg egy páros számot! "))
+folytat = False
+
+
+while folytat == False:
+    fsz_input = int(input("Kérlek adj meg egy páros számot! "))
+    if (fsz_input % 2 == 0):
+        print("Köszönöm szépen te majom xddd")
+        folytat = True
+
+
+
